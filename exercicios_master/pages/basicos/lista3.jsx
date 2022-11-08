c8ebca08c0e213b2eba46bd33ae4d2585694dd1e
Matheus Bonjour <matheus.bonjour@gmail.com>
2022-11-08 19:29:27 -0300

    novo

---
+++ exercicios_master/pages/basicos/lista3.jsx
@@ -0,0 +1,21 @@
+function gerarLista(final = 10) {
+    const lista = []
+    for(let i = 1; i <= final; i++) {
+        lista.push(<span>{i},</span> )
+    }
+    return lista
+}    
+
+export default function lista() {
+    return (
+    <div> 
+        <div>
+            {gerarLista(30)}
+        </div>
+        <div>
+            {gerarLista(70)}
+        </div>
+    </div>
+    )
+}
+
---
+++ exercicios_master/pages/css/integracao1.jsx
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+export default function integracao() {
+    return (
+        <div>
+            <h1>minha pomba</h1>
+            <h2>titulo 2</h2>
+            <h3>titulo3</h3>
+        </div>
+    )
+}
