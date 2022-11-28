965dbfe14eefa8972d148b435b8098cbd43c4cf6
matheusbonjour <matheusbonjour@users.noreply.github.com>
2022-11-28 11:30:28 -0300

    novo

---
+++ exercicios_master/pages/estado/mouse.jsx
@@ -0,0 +1,13 @@
+export default function mouse(prop) {
+    return (
+        <div style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#222",
+            color: "#fff"
+        }} >
+            mouse
+        </div>
+    )
+}
